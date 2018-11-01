d68f907929252b5a1dbbc3864f7a5368ea894923
jovixv <igrolan@gmail.com>
2018-11-01 16:42:31 +0200

    Change example RelatedKeywords

--- public/Examples/KeywordsFinderApi/Related_Keywords/RelatedKeywords.php
+++ public/Examples/KeywordsFinderApi/Related_Keywords/RelatedKeywords.php
@@ -18,7 +18,8 @@ $completed  = $model->setOpt('keyword','big data')
 if (!$completed->isSuccessFul())
     dd($completed);
 
-dd($completed);
+//dd($completed); // - This method can be used to get a list of all available parameters in the form of an object.The data is serialized and similar to the response of DFS API.
+
 // you can call property as described below
 echo 'status: '       .$completed->status        ."<br>";
 echo 'results_time: ' .$completed->results_time  ."<br>";

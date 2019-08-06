5351ef733dd9fbb16840aa56de929361bbf34533
Mehul V <mehulvadodariya2012@gmail.com>
2019-08-06 14:36:52 +0530

    Added Google Image search API

---
+++ DFSClient/Models/SERP_API/GetImageCompletedTasks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DFSClient\Models\SERP_API;
+
+use DFSClient\Models\AbstractModel;
+
+class GetImageCompletedTasks extends AbstractModel
+{
+    protected $requestToFunction = 'srp_google_images_tasks_get';
+    protected $pathToMainData = 'results';
+    protected $method = 'GET';
+    protected $isSupportedMerge = false;
+}

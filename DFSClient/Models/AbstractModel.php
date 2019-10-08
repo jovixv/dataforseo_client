b53539ab86058a525cebdbe33c61561479fa9817
jovixv <igrolan@gmail.com>
2019-10-08 15:08:52 +0300

    fixed async method

--- DFSClient/Models/AbstractModel.php
+++ DFSClient/Models/AbstractModel.php
@@ -246,6 +246,7 @@ abstract class AbstractModel
         $resultsArray = [];
 
         foreach ($pool as $key=>$val) {
+            $finishedPayload['json']['data'] = null;
             $payLoad = $val->queryBuilder->getPayload();
             $header = $val->headers;
 
